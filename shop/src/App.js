cbc00a2aec4a20418a4245b369b6317559949249
bewisessong <bewisessong@gmail.com>
2022-01-15 22:02:22 +0900

    01/15(토) : 상품 Component화+props+map

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data';
+import Card from './Card';
+
 
 function App() {
 
   /* 01/15(토) : import/export 사용하기 */
   let [shoes, setShoes] = useState(Data);
-  console.log(shoes);
   
   return (
     /* 01/15(토) : 쇼핑몰 레이아웃 디자인 */
@@ -41,24 +42,14 @@ function App() {
         </div>
       </div>
 
-      {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 */}
+      {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 row와 col 사용*/}
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content} {shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].content} {shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].content} {shoes[2].price}</p>
-          </div>
+          {
+            shoes.map((shoe, i)=> {
+              return <Card shoes={shoe} index={i} />
+            })
+          }
         </div>
       </div>
     </div>
