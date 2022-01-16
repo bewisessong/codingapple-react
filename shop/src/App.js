6cdbe04ca4e058b612f68fa3a8bede78f47330ab
bewisessong <bewisessong@gmail.com>
2022-01-16 21:30:05 +0900

    01/16(일) : React Router 2 - Link, Switch, history 기능

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,6 +4,7 @@ import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data';
 import Card from './Card';
+import Detail from './Detail';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -21,8 +22,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+              <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -35,43 +36,38 @@ function App() {
         </Container>
       </Navbar>
 
-      {/* 01/16(일) : React Router 1 - 세팅과 기본 라우팅 */}
-      <Route exact path="/">
-        <div className="container-fluid text-light p-5">
-          <div className="container p-5">
-              <h1 className="display-4">20% Season Off</h1>
-              <p>Go to My Website</p>
-              <Button variant="primary">Learn more</Button>{' '}
-          </div>
-        </div>
-
-        {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 row와 col 사용*/}
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((shoe, i)=> {
-                return <Card shoes={shoe} index={i} />
-              })
-            }
-          </div>
-        </div>
-      </Route>
-
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
+      {/* 01/16(일) : React Router 2 - Link, Switch, history 기능 */}
+      <Switch>
+        {/* 01/16(일) : React Router 1 - 세팅과 기본 라우팅 */}
+        <Route exact path="/">
+          <div className="container-fluid text-light p-5">
+            <div className="container p-5">
+                <h1 className="display-4">20% Season Off</h1>
+                <p>Go to My Website</p>
+                <Button variant="primary">Learn more</Button>{' '}
             </div>
           </div>
-        </div> 
-      </Route>
+
+          {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 row와 col 사용*/}
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((shoe, i)=> {
+                  return <Card shoes={shoe} index={i} />
+                })
+              }
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail">
+          <Detail />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나적었을 때 이거 보여주세용</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
