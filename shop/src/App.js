8e1df0509f6d73a8cc4dceab119766ba564fb055
bewisessong <bewisessong@gmail.com>
2022-01-16 21:08:06 +0900

    01/16(일) : React Router 1 - 세팅과 기본 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Data from './data';
 import Card from './Card';
 
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
@@ -34,24 +35,43 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="container-fluid text-light p-5">
-        <div className="container p-5">
-            <h1 className="display-4">20% Season Off</h1>
-            <p>Go to My Website</p>
-            <Button variant="primary">Learn more</Button>{' '}
+      {/* 01/16(일) : React Router 1 - 세팅과 기본 라우팅 */}
+      <Route exact path="/">
+        <div className="container-fluid text-light p-5">
+          <div className="container p-5">
+              <h1 className="display-4">20% Season Off</h1>
+              <p>Go to My Website</p>
+              <Button variant="primary">Learn more</Button>{' '}
+          </div>
         </div>
-      </div>
 
-      {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 row와 col 사용*/}
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((shoe, i)=> {
-              return <Card shoes={shoe} index={i} />
-            })
-          }
+        {/* 웹 화면에서는 가로로, 앱 화면에서는 세로로 보이게 하고싶을 때 row와 col 사용*/}
+        <div className="container">
+          <div className="row">
+            {
+              shoes.map((shoe, i)=> {
+                return <Card shoes={shoe} index={i} />
+              })
+            }
+          </div>
         </div>
-      </div>
+      </Route>
+
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+      </Route>
     </div>
   );
 }
